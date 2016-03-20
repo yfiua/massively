6cffe7f2c92b40ca03959670e0aaf8c46ec5e70c
Yfiua <yfiua@jun-mbp13.home>
2016-03-20 19:30:28 +0100

    install.sh for mac

---
+++ mac/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# .bash_profile
+if [ -f ~/.bash_profile ]; then
+    echo "File ~/.bash_profile already exists! Saving to ~/.bash_profile.old"
+    mv ~/.bash_profile ~/.bash_profile.old
+    echo "Merging automatically ..."
+    cat ~/.bash_profile.old .bash_profile > ~/.bash_profile
+    echo "Merged successfully! You may need to check it manually by:"
+    echo "vi ~/.bash_profile"
+else
+    cp .bash_profile ~/.bash_profile
+    echo "~/.bash_profile created successfully!"
+fi
+
+# key bindings
+if [ ! -d ~/Library/KeyBindings ]; then
+    mkdir ~/Library/KeyBindings
+fi
+cp ./DefaultKeyBinding.dict ~/Library/KeyBindings
+
+echo "Success! (if u don't see an error)"
+
