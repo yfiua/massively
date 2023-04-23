a0fc04cd650c8a4e58e7a26cc3c67d1337dab949
Jun Sun <yfiua@qq.com>
2023-04-23 17:03:11 +0200

    move vim files to individual repo

--- vim/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-cp vimrc ~/.vimrc
-
-# Install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "Success! (if u don't see an error)"
-
--- vim/update.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cp ~/.vimrc vimrc
-echo "Update successfully (if u don't see an error)! Note you should manually commit and push the vimrc file."
